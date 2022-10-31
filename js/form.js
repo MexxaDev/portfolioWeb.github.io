31b04cd54c6e0536d64bba6ba2ce3217e9db0eac
Nahuel Beeguer <44383860+MexxaDev@users.noreply.github.com>
2022-10-31 16:28:52 -0300

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,9 +1,11 @@
-const nombre = document.querySelector('#');
-const correo = document.querySelector('#');
-const mensaje = document.querySelector('#');
-const btnEnviar = document.querySelector('#');
+const nombre = document.querySelector('#nombre');
+const correo = document.querySelector('#correo');
+const mensaje = document.querySelector('#mensaje');
+const btnEnviar = document.querySelector('#btnEnviar');
+
 var WhatsappMensaje;
 
 function enviar () {
-    WhatsappMensaje = `Nombre:  ${nombre}  correo:  ${correo} mensaje: ${mensaje}`
+    WhatsappMensaje = `https://api.whatsapp.com/send?phone=543496578813&text=Nombre:%20${nombre.value}%20correo:%20${correo.value}%20mensaje:%20${mensaje.value}`
+    btnEnviar.href = WhatsappMensaje;
 }
